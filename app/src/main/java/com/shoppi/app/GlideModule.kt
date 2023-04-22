d512095959950ee25221d2392946d1494a3b3e0e
Jdoublee <hyewon3429@gmail.com>
2023-04-22 23:59:52 +0900

    섹션7. 홈화면 Appbar UI 구현

---
+++ app/src/main/java/com/shoppi/app/GlideModule.kt
@@ -0,0 +1,9 @@
+package com.shoppi.app
+
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.annotation.GlideModule
+
+@GlideModule
+class GlideModule: AppGlideModule() {
+
+}
